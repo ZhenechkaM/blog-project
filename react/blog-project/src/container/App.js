1aea82f9644fdfb622b2156d55bbe134cb1e7862
Matvienko <e.motko@gmail.com>
2021-08-09 10:53:53 +0200

    created the site  structure

---
+++ react/blog-project/src/container/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header.js'
+import Slider from '../components/Slider.js'
+import Main from '../components/Main.js'
+import Footer from '../components/Footer.js'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Slider />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
